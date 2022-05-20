ae855e1c44a3089f3119e278c80e8a8b9b216791
SaudZafeer <105937990+SaudZafeer@users.noreply.github.com>
2022-05-20 13:01:56 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def check_prime(number : int):
+    flag = False
+    if number > 1:
+        for previous_numbers in range(2,number):
+            if (number % previous_numbers) == 0:
+                flag = True
+                break
+    if flag:
+        print('Not a prime number') 
+    else:
+        print('Prime number')    
+
+user_input = int(input('please enter a numebr to check if it is prime or not: '))
+check_prime(user_input)
